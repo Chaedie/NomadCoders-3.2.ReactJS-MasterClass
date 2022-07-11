c783c338c1ed2bddeab1b431deb0c7911a9db52c
Chaedie <chaedong.im.dev@gmail.com>
2022-07-11 10:54:56 +0900

    learn : Styled Componets 학습

--- src/App.js
+++ src/App.js
@@ -1,10 +1,47 @@
+import styled from 'styled-components';
+
+const Father = styled.div`
+  display: flex;
+`;
+
+const Box = styled.div`
+  background-color: ${(props) => props.bgColor};
+  width: 10rem;
+  height: 10rem;
+`;
+
+const Circle = styled(Box)`
+  border-radius: 5rem;
+`;
+
+const Btn = styled.button`
+  color: white;
+  background-color: tomato;
+  border: 0;
+  border-radius: 15px;
+`;
+
+const Input = styled.input.attrs({ required: true, minLength: 10 })`
+  background-color: tomato;
+`;
+
+const Text = styled.h3`
+  color: white;
+`;
+
 function App() {
   return (
-    <div>
-      <div></div>
-      <div></div>
-      <h1>Let's start!</h1>
-    </div>
+    <Father as="header">
+      <Input></Input>
+      <Input></Input>
+      {/* <Box bgColor="teal">
+        <Text>Let's start!</Text>
+      </Box>
+      <Circle bgColor="tomato" /> */}
+      <Btn as="a" href="/">
+        Log in
+      </Btn>
+    </Father>
   );
 }
 
