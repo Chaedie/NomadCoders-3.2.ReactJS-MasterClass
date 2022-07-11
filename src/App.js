2595bfb25b0aa9a978cab184b8ee9a2718800e49
Chaedie <chaedong.im.dev@gmail.com>
2022-07-11 11:38:05 +0900

    learn : Styled Component - pseudo Selector 학습

--- src/App.js
+++ src/App.js
@@ -1,47 +1,59 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 
-const Father = styled.div`
+const Wrapper = styled.div`
   display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  width: 100vw;
+`;
+
+const rotateAnimation = keyframes`
+  0% {
+    transform : rotate(0deg);
+  }
+  50% {
+    transform : rotate(360deg);
+    border-radius: 100px
+  }
+  100%{
+    transform : rotate(720deg);
+    border-radius: 0;
+  }
 `;
 
 const Box = styled.div`
-  background-color: ${(props) => props.bgColor};
-  width: 10rem;
-  height: 10rem;
-`;
-
-const Circle = styled(Box)`
-  border-radius: 5rem;
-`;
-
-const Btn = styled.button`
-  color: white;
+  height: 200px;
+  width: 200px;
   background-color: tomato;
-  border: 0;
-  border-radius: 15px;
-`;
-
-const Input = styled.input.attrs({ required: true, minLength: 10 })`
-  background-color: tomato;
-`;
-
-const Text = styled.h3`
-  color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  animation: ${rotateAnimation} 2s linear infinite;
+  span {
+    font-size: 36px;
+    &:hover {
+      font-size: 70px;
+    }
+    &:active {
+      opacity: 0;
+    }
+  }
+  /* span:hover {
+    font-size: 70px;
+  }
+  span:active {
+    opacity: 0;
+  } */
 `;
 
 function App() {
   return (
-    <Father as="header">
-      <Input></Input>
-      <Input></Input>
-      {/* <Box bgColor="teal">
-        <Text>Let's start!</Text>
+    <Wrapper>
+      <Box>
+        <p>🤣</p>
       </Box>
-      <Circle bgColor="tomato" /> */}
-      <Btn as="a" href="/">
-        Log in
-      </Btn>
-    </Father>
+    </Wrapper>
   );
 }
 
