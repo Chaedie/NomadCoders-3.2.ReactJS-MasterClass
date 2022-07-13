73cd438135cf5512dee2fcf6ad2cd5ecb13d9ba9
Chaedie <chaedong.im.dev@gmail.com>
2022-07-13 16:35:13 +0900

    learn : Typescript에서의 useState 사용 학습

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
-import Circle from './Circle';
+import { useState } from 'react';
 
 function App() {
+  const [value, setValue] = useState('');
+  const onChange = (event: React.FormEvent<HTMLInputElement>) => {
+    const {
+      currentTarget: { value },
+    } = event;
+    setValue(value);
+  };
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log('Hello', value);
+  };
   return (
     <div>
-      <Circle bgColor="teal" borderColor="yellow"></Circle>
-      <Circle bgColor="tomato"></Circle>
+      <form onSubmit={onSubmit} action="">
+        <input type="text" onChange={onChange} placeholder="username" />
+        <button>Log in</button>
+      </form>
     </div>
   );
 }
